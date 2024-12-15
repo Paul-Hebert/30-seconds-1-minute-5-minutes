fb867e7f519f85ea16e783adfa55080e8a7296dd
Paul Hebert <paul@cloudfour.com>
2024-12-15 10:54:20 -0800

    Add sounds and fonts

--- app/add-drawing.tsx
+++ app/add-drawing.tsx
@@ -1,10 +1,11 @@
 import { View } from "react-native";
 import Timer from "@/components/Timer";
+import { useState } from "react";
 
 export default function AddDrawingScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Timer count={30} />
+      <Timer count={5 * 60} onFinished={() => {}} />
     </View>
   );
 }
--- components/Timer.tsx
+++ components/Timer.tsx
@@ -2,7 +2,15 @@ import { View, StyleSheet } from "react-native";
 import { Text } from "./StyledText";
 import { Button } from "./Button";
 import { useEffect, useState } from "react";
+import { Audio } from "expo-av";
+import { useFonts, RobotoMono_700Bold } from "@expo-google-fonts/roboto-mono";
 import Heading from "./Heading";
+
+function formatTime(seconds: number): string {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  return `${minutes}:${remainingSeconds.toString().padStart(2, "0")}`;
+}
 
 type TimerProps = {
   count: number;
@@ -12,6 +20,27 @@ type TimerProps = {
 export default function Timer({ count, onFinished }: TimerProps) {
   const [currentTime, setCurrentTime] = useState(count);
   const [isRunning, setIsRunning] = useState(false);
+  const [sound, setSound] = useState<Audio.Sound>();
+  const [loaded] = useFonts({
+    RobotoMono_700Bold,
+  });
+
+  async function playSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      require("../assets/sounds/timer-end.mp3"),
+      { volume: 1 }
+    );
+    setSound(sound);
+    await sound.playAsync();
+  }
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
 
   useEffect(() => {
     if (isRunning) {
@@ -19,9 +48,11 @@ export default function Timer({ count, onFinished }: TimerProps) {
         setCurrentTime((prevTime) => {
           if (prevTime === 1) {
             setIsRunning(false);
+            playSound();
             onFinished();
             return 0;
           }
+
           return prevTime - 1;
         });
       }, 1000);
@@ -29,9 +60,13 @@ export default function Timer({ count, onFinished }: TimerProps) {
     }
   }, [isRunning]);
 
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.count}>{currentTime}</Text>
+      <Text style={styles.count}>{formatTime(currentTime)}</Text>
       <Button
         onPress={() => {
           setIsRunning(!isRunning);
@@ -49,7 +84,7 @@ const styles = StyleSheet.create({
     gap: 16,
   },
   count: {
-    fontSize: 160,
-    fontWeight: "bold",
+    fontSize: 144,
+    fontFamily: "RobotoMono_700Bold",
   },
 });
