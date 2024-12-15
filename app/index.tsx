7b46848fb2fe474b53db2332c892d92618f64d1d
Paul Hebert <paul@cloudfour.com>
2024-12-15 10:01:40 -0800

    Begin tidying repo and adding base styles

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import Heading from "@/components/Heading";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Heading>Let's Draw!</Heading>
+    </View>
+  );
+}
